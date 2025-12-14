2ab92c2eca7724971da475b70c23c5df419c4fef
ZiadHamdyy <ziadhamdy667@gmail.com>
2025-12-14 19:50:39 +0200

    feat: integrate SubscriptionModule into PayableAccountModule and enhance PayableAccountService with subscription limit enforcement during account creation

--- stockpro-backend/src/modules/payable-account/payable-account.module.ts
+++ stockpro-backend/src/modules/payable-account/payable-account.module.ts
@@ -3,9 +3,10 @@ import { DatabaseModule } from '../../configs/database/database.module';
 import { PayableAccountService } from './payable-account.service';
 import { PayableAccountController } from './payable-account.controller';
 import { FiscalYearModule } from '../fiscal-year/fiscal-year.module';
+import { SubscriptionModule } from '../subscription/subscription.module';
 
 @Module({
-  imports: [DatabaseModule, FiscalYearModule],
+  imports: [DatabaseModule, FiscalYearModule, SubscriptionModule],
   controllers: [PayableAccountController],
   providers: [PayableAccountService],
   exports: [PayableAccountService],
